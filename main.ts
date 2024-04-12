3ac99db1c2f05a593dcf300fabe98dd22baeb9a8
architectureYK <mahmedyk789@gmail.com>
2024-04-12 23:18:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+
+let conversion :any= {
+    PKR : {
+        USD : 0.0045,
+        EUR : 0.0040,
+        PKR : 1
+    },
+    USD : {
+       USD : 1,
+       EUR : 0.8,
+       PKR : 282
+    },
+    EUR : {
+        USD : 1.2,
+        EUR : 1,
+        PKR : 340
+    }
+}
+function currencyConverter(){
+    console.log("Welcome to currency converter");
+  inquirer.prompt([
+    {
+        type : "input",
+        name : "amount",
+        message : "Enter amount :"
+    },
+    {
+        type : "list",
+        name : "from",
+        message : "From :",
+        choices:["PKR","USD","EUR"]
+    },
+    {
+        type : "list",
+        name : "to",
+        message : "To :",
+        choices:["PKR","USD","EUR"]
+    }
+ ]).then((answer)=>{
+     let convertedAmount = conversion[answer.from][answer.to] * answer.amount
+     if(typeof convertedAmount === "number"){
+         console.log(answer.amount + " " + answer.from + " = " + convertedAmount + " " + answer.to)
+     if(convertedAmount == -1){
+        console.log("Invalid currency")
+     }
+    }
+    else{
+        console.log("Invalid amount")
+    }
+ })
+}
+currencyConverter()
